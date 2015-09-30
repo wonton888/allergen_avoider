19d608f291dd0d1ce662f10a97a4498501742c39
Marcos Moncivais <marcos.moncivais@gmail.com>
2015-09-30 11:13:10 -0700

    add routes and html and css

---
+++ app/app.php
@@ -0,0 +1,30 @@
+<?php
+    require_once __DIR__."/../vendor/autoload.php";
+    require_once __DIR__."/../src/Restaurant.php";
+    require_once __DIR__."/../src/Allergen.php";
+
+    // Add symfony debug component and turn it on.
+    use Symfony\Component\Debug\Debug;
+    Debug::enable();
+
+    $app = new Silex\Application();
+
+    // Set Silex debug mode in $app object
+    $app['debug'] = true;
+
+    $server = 'mysql:host=localhost;dbname=inventory';
+    $username = 'root';
+    $password = 'root';
+    $DB = new PDO($server, $username, $password);
+
+    $app->register(new Silex\Provider\TwigServiceProvider(), array(
+        'twig.path' => __DIR__.'/../views'
+    ));
+
+    $app->get('/', function() use ($app){
+        return $app['twig']->render('index.html.twig', array('allergens' => Allergen::getAll()));
+    });
+
+    return $app;
+
+?>
